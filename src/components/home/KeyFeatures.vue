cd9d0a42d5c0d9ac86ae824c7117213fb4aa8722
majordutch <354294+majordutch@users.noreply.github.com>
2021-05-19 19:46:43 -0700

    Break up some modules on home page

---
+++ src/components/home/KeyFeatures.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="relative bg-white py-16 sm:py-24 lg:py-32">
+    <div
+      class="
+        mx-auto
+        max-w-md
+        px-4
+        text-center
+        sm:max-w-3xl
+        sm:px-6
+        lg:px-8
+        lg:max-w-7xl
+      "
+    >
+      <h2
+        class="text-base font-semibold tracking-wider text-cyan-600 uppercase"
+      >
+        Key Features
+      </h2>
+      <p
+        class="
+          mt-2
+          text-3xl
+          font-extrabold
+          text-gray-900
+          tracking-tight
+          sm:text-4xl
+        "
+      >
+        Promote the platform you're invested in
+      </p>
+      <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+        Convincing traders to move platforms is a tough task, but these key
+        features can be used as a tool to educate and inform.
+      </p>
+      <div class="mt-12">
+        <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          <div v-for="feature in features" :key="feature.name" class="pt-6">
+            <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+              <div class="-mt-6">
+                <div>
+                  <span
+                    class="
+                      inline-flex
+                      items-center
+                      justify-center
+                      p-3
+                      bg-gradient-to-r
+                      from-teal-500
+                      to-cyan-600
+                      rounded-md
+                      shadow-lg
+                    "
+                  >
+                    <font-awesome-icon
+                      :icon="feature.icon"
+                      class="text-white"
+                      size="lg"
+                      fixed-width
+                    />
+                  </span>
+                </div>
+                <h3
+                  class="mt-8 text-lg font-medium text-gray-900 tracking-tight"
+                >
+                  {{ feature.name }}
+                </h3>
+                <p class="mt-5 text-base text-gray-500">
+                  {{ feature.description }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const features = [
+  {
+    name: "Decentralized Exchange (Dex)",
+    description:
+      "A fully decentralized, censorship resistant platform that supports any type of financial market.",
+    icon: ["fas", "globe"],
+  },
+  {
+    name: "Spot & Futures Trading",
+    description:
+      "All the familiar features from centralized exchanges, on-chain and verifiable with low taker fees.",
+    icon: ["fas", "balance-scale"],
+  },
+  {
+    name: "On-boarding & Security",
+    description:
+      "Your keys, your crypto. Always have complete ownership of your funds and get started in less than a minute.",
+    icon: ["fas", "shield-check"],
+  },
+  {
+    name: "Ultimate Privacy",
+    description:
+      "Your personal information is never required to trade or transfer, so it can never be shared with a 3rd-party or stolen by bad actors.",
+    icon: ["fas", "user-secret"],
+  },
+  {
+    name: "Fair & Transparent Trading",
+    description:
+      "All trading activity is recorded transparently on the blockchain and order execution is randomized to prevent front-running.",
+    icon: ["fas", "cube"],
+  },
+];
+
+export default {
+  setup() {
+    return {
+      features,
+    };
+  },
+};
+</script>
---
+++ src/components/shared/CtaSwag.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="relative bg-gray-900">
+    <div
+      class="
+        relative
+        h-56
+        bg-cyan-600
+        sm:h-72
+        md:absolute
+        md:left-0
+        md:h-full
+        md:w-1/2
+      "
+    >
+      <img
+        class="w-full h-full object-cover"
+        src="https://images.unsplash.com/photo-1618487327695-db3012405065?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+        alt=""
+      />
+      <div
+        aria-hidden="true"
+        class="absolute inset-0 bg-gradient-to-r from-teal-500 to-cyan-600"
+        style="mix-blend-mode: multiply"
+      />
+    </div>
+    <div
+      class="
+        relative
+        mx-auto
+        max-w-md
+        px-4
+        py-12
+        sm:max-w-7xl
+        sm:px-6
+        sm:py-20
+        md:py-28
+        lg:px-8
+        lg:py-32
+      "
+    >
+      <div class="md:ml-auto md:w-1/2 md:pl-10">
+        <h2
+          class="text-base font-semibold uppercase tracking-wider text-gray-300"
+        >
+          Need swag?
+        </h2>
+        <p
+          class="
+            mt-2
+            text-white text-3xl
+            font-extrabold
+            tracking-tight
+            sm:text-4xl
+          "
+        >
+          We’ve got you covered
+        </p>
+        <p class="mt-3 text-lg text-gray-300">
+          Our community artists have created some uniquely awesome designs to
+          help promote products like Demex. From time to time, we offer physical
+          marketing materials <em>(free of charge!)</em> with support from our
+          sponsors.
+        </p>
+        <div class="mt-8">
+          <div class="inline-flex rounded-md shadow">
+            <router-link
+              :to="{ name: 'swag' }"
+              class="
+                inline-flex
+                items-center
+                justify-center
+                px-5
+                py-3
+                border border-transparent
+                text-base
+                font-medium
+                rounded-md
+                text-gray-900
+                bg-white
+                hover:bg-gray-50
+              "
+            >
+              Get some swag
+              <font-awesome-icon
+                :icon="['fas', 'external-link']"
+                class="-mr-1 ml-3 text-gray-400"
+              />
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
